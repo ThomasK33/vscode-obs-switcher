903262cf139424bc620ab693897435ce9ae05f02
quiquelhappy <quiquelhappy@gmail.com>
2023-04-09 13:56:53 +0200

    minor import and type fixes

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-import * as minimatch from "minimatch";
+import minimatch from "minimatch";
 import { extensionKey } from "./constants";
 import { ConfigManager } from "./services/configuration";
 import { OBSManager } from "./services/obsmanager";
